a12c1c1b03c94dc679a73ef7ef8b49ac4186c0a1
Lakshay Newatia <141578754+lakshayNewatia@users.noreply.github.com>
2026-01-13 13:01:03 +0530

    Update App.py

--- App.py
+++ App.py
@@ -10,6 +10,7 @@ import base64, random
 import time, datetime
 import pymysql
 import os
+import getpass
 import geocoder
 import io
 import plotly.express as px 
@@ -242,7 +243,7 @@ def run():
         sec_token = secrets.token_urlsafe(12)
         host_name = socket.gethostname()
         ip_add = socket.gethostbyname(host_name)
-        dev_user = os.getlogin()
+        dev_user = getpass.getuser()
         os_name_ver = platform.system() + " " + platform.release()
         g = geocoder.ip('me')
         latlong = g.latlng
